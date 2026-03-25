8a03c45ec3f0a61e95935cb3ee82c491a36e925b
Simal Chaudahri <simalchaudhari20496@gmail.com>
2026-03-25 10:24:09 +0530

    add

---
+++ event-web/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            DEFAULT: '#0f766e',
+            light: '#0d9488',
+          },
+        },
+        fontFamily: {
+          sans: ['Inter', 'system-ui', 'sans-serif'],
+        },
+        maxWidth: {
+          'app': '480px',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
